ac55403e917f39612f5cffe7c04f2a32ce538847
Ali Altaf <alialtaf@seas.upenn.edu>
2013-09-06 20:49:48 -0400

    initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudySpaces::Application.config.secret_token = '9b7595dd8002e35766d07b6183838bcb0393fc59f165f6ab878f902dc434358a06d1ad2ac9ce065b052f0d26e623bc8521fb204f1151d8ca68b2d48c49ade647'
