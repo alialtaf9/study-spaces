ad3c6bec1fa481b754613c97a984d1d6a9355d2b
Susan Greenberg <susangr@seas.upenn.edu>
2013-09-07 06:30:27 -0400

    gems

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+CSV.foreach("/Users/susangreenberg/Documents/junior/pennapps/mainbuildings.csv") do |row| 
+  m = Building.new
+  m.update_attributes(:name => row[0], :address => row[1])
+end
+
+CSV.foreach("/Users/susangreenberg/Documents/junior/pennapps/studylocations.csv") do |row| 
+  building = Building.find_by_name(row[1])
+  if (building != nil)
+    building.rooms.create(:name => row[0])
+  end
+end
